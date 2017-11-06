9e489d6b4ec8822a45086e31b1b142046fa7cc4e
ericaddison <ericaddison@gmail.com>
2017-11-05 23:22:37 -0600

    Refactored prototype API endpoint style

--- source/api/get_conversations.py
+++ source/api/get_conversations.py
@@ -3,17 +3,17 @@ from source.framework.BaseHandler import BaseHandler
 
 def get_conversations(names=None):
     """Get conversations by name, or get all conversations if no names provided"""
+    response = {}
     if names is None:
-        return "All conversations"
+        response['conversations'] = "All conversations"
     else:
-        return "Requested conversations"
+        response['conversations'] = "Requested conversations"
+    return response
 
 
 class GetConversations(BaseHandler):
     """API handler to allow getting conversation data"""
     def get(self):
         self.set_content_text_json()
-        response = {}
-
-        response['conversations'] = get_conversations()
+        response = get_conversations()
         self.write_dictionary_response(response)
