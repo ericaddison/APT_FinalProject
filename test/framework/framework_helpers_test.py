357bc7c2d2b136cc61565c494f3aa470ae5c0b48
ericaddison <ericaddison@gmail.com>
2017-11-05 22:20:05 -0600

    Added unit test for framework_headers.make_routes()

---
+++ test/framework/framework_helpers_test.py
@@ -0,0 +1,16 @@
+import unittest
+import source.framework.framework_helpers as fh
+
+
+class TestWebAppMethods(unittest.TestCase):
+
+    def test_make_routes(self):
+        route = '/route1'
+        app = fh.make_routes([(route, self.__class__)])
+        assert app is not None
+        assert route == app.router.match_routes[0].template
+        assert self.__class__ == app.router.match_routes[0].handler
+
+
+if __name__ == '__main__':
+    unittest.main()
