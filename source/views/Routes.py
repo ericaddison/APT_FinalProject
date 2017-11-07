f92c29048d4b5259add0a14572d4dc3d662b0272
ericaddison <ericaddison@gmail.com>
2017-11-06 22:50:32 -0600

    Added basic auth0 example

--- source/views/Routes.py
+++ source/views/Routes.py
@@ -1,7 +1,13 @@
 from source.framework.webapp2_helpers import make_routes
 from source.views.Main import LandingPage
+from auth0_demo import Auth0LoginCallback, Auth0MainPage
 
 app = make_routes([
+    # [START auth0 demo]
+    ('/auth0demo/', Auth0MainPage),
+    ('/auth0demo/callback', Auth0LoginCallback),
+    # [END auth0 demo]
+
     # [START views]
     ('/.*', LandingPage)
     # [END views]
