60cbc11bb5be71358b09c88fecbbecf904aeb2d3
ericaddison <ericaddison@gmail.com>
2017-11-07 21:18:26 -0600

    Added messages API

--- source/framework/constants.py
+++ source/framework/constants.py
@@ -1,4 +1,6 @@
 # [BEGIN api request parms]
 redirect_url_parm = "redirect"
 auth_token_parm = "token"
+conversastion_name_parm = "conv_name"
+conversastion_id_parm = "conv_id"
 # [END api request parms]
