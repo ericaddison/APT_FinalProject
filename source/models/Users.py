af0a09937828487b14fb909ed4bc795fb9d25f1e
ericaddison <ericaddison@gmail.com>
2017-11-08 16:26:44 -0600

    Added data_dict and create for Users model

--- source/models/Users.py
+++ source/models/Users.py
@@ -4,7 +4,7 @@ class Users(ndb.Model):
     email = ndb.StringProperty(indexed=True)
     fName = ndb.StringProperty()
     lName = ndb.StringProperty()
-    joinDate = ndb.DateTimeProperty()
+    joinDate = ndb.DateTimeProperty(auto_now_add=True)
     prefComm = ndb.StringProperty() #Preferred Communication Method
 
 
@@ -15,3 +15,20 @@ class Users(ndb.Model):
         #if (id is integer, then it's the userID)
         #else (id is email)
         return #details on a specific user
+
+    def get_user_data_dict(self):
+        return {"email": self.email,
+                "fName": self.fName,
+                "lName": self.lName,
+                "joinDate": self.joinDate,
+                "prefComm": self.prefComm}
+
+    @classmethod
+    def create(cls, email, fname, lname, prefcomm=""):
+        user = Users(email=email,
+                     fname=fname,
+                     lName=lname,
+                     prefComm=prefcomm
+                     )
+        user.put()
+        return user
