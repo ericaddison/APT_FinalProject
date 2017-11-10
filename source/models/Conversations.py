674d02f57cd55730308903bdb3c786c4764ae7bc
ericaddison <ericaddison@gmail.com>
2017-11-09 22:00:57 -0600

    updated conversatiosn model

--- source/models/Conversations.py
+++ source/models/Conversations.py
@@ -1,33 +1,40 @@
 from google.appengine.ext import ndb
+import datetime
+
 
 class Conversations(ndb.Model):
     owner = ndb.KeyProperty(indexed=True, kind='Users')
     name = ndb.StringProperty()
     password = ndb.StringProperty() #bcrypt hash value
     createDate = ndb.DateTimeProperty()
-    destroyDate = ndb.DateTimeProperty()
+    destroyDate = ndb.DateTimeProperty(indexed=True)
     users = ndb.KeyProperty(repeated=True, kind='Users')
     idPolicy = ndb.StringProperty() #???
     viewAfterExpire = ndb.BooleanProperty()
     revealOwner = ndb.BooleanProperty()
     restrictComms = ndb.StringProperty() #???
 
-    def get_all_conversations(self):
-        return #list of all conversations
-
-    def get_all_active_conversations(self):
-        return #list of non-destroyed conversations
-
-    def get_conversation_by_id(self, convID):
-        return #conv details by conv ID
-
-    def get_conversation_by_owner(self, ownerID):
-        return #conv details by owner's user ID
-
-    def get_conversation_users(self, convID):
-        return #list of users in a specific conversation
+    def get_conversation_users(self):
+        return self.users
 
     def check_conversation_password(self, convID, passwordHash):
         #use bcrypt to check pw hash param against stored
         #conversation password value
-        return #boolean
+        return False
+
+    @classmethod
+    def get_all_conversations(self):
+        return Conversations.query().fetch()
+
+    @classmethod
+    def get_all_active_conversations(self):
+        return Conversations.query(Conversations.destroyDate > datetime.datetime.now())
+
+    @classmethod
+    def get_conversation_by_id(self, convID):
+        return ndb.Key('Conversations', long(convID)).get()
+
+    @classmethod
+    def get_conversation_by_owner(self, ownerID):
+        return ndb.query(Conversations.owner == ownerID)
+
