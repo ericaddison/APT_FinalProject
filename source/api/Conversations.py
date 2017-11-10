d606f9a7b80fd1ab270302753c9a2203b4684219
ericaddison <ericaddison@gmail.com>
2017-11-09 23:04:43 -0600

    Added create() and more helpers to Conversations

--- source/api/Conversations.py
+++ source/api/Conversations.py
@@ -1,5 +1,8 @@
-from source.framework.ApiServiceHandler import ApiServiceHandler
-
+from source.framework.ApiServiceHandler import ApiServiceHandler, NOT_FOUND_RESPONSE
+import source.framework.constants as c
+from source.models.Conversations import Conversations, id_policies
+from datetime import datetime
+import time
 
 # [BEGIN API python methods]
 
@@ -13,11 +16,31 @@ def get_conversations(user, conv_id):
     return response
 
 
-def create_conversation(user, conv_id):
+def create_conversation(user, name, destroy_date, id_policy, view_after_expire, reveal_owner, restrict_comms, password_hash):
     """Create a new conversation"""
-    response = {}
-    response['conversations'] = "New conversation"
-    return response
+    # check user authorization
+    # check_user_auth()...
+
+    # default values
+    name = name if name else Conversations.random_name()
+    id_policy = id_policy if id_policy in id_policies else "colors"
+    destroy_date = destroy_date if destroy_date else datetime.fromtimestamp(time.time()+90*24*3600)
+    destroy_date = destroy_date if destroy_date>datetime.now() else datetime.fromtimestamp(time.time()+90*24*3600)
+    view_after_expire = False if view_after_expire.lower() == 'false' else True
+    reveal_owner = False if reveal_owner.lower() == 'false' else True
+    restrict_comms = restrict_comms if restrict_comms else ""
+    password_hash = password_hash if password_hash else ""
+
+    conv = Conversations.create(name=name,
+                                owner=user,
+                                destroy_date=destroy_date,
+                                id_policy=id_policy,
+                                view_after_expire=view_after_expire,
+                                reveal_owner=reveal_owner,
+                                restrict_comms=restrict_comms,
+                                password_hash=password_hash)
+
+    return {'conversations': conv.get_full_data(), 'status': 200}
 
 
 def update_conversation(user, conv_id):
@@ -47,7 +70,22 @@ class ConversationsApi(ApiServiceHandler):
 
     def post_hook(self, user, url_id):
         """Create conversation data API"""
-        return create_conversation(user)
+        if url_id != "":
+            return NOT_FOUND_RESPONSE
+
+        # dummy version, for now
+        name = self.get_request_param(c.conversastion_name_parm)
+        destroy_date = self.get_request_param(c.destroydate_parm)
+        id_policy = self.get_request_param(c.idpolicy_parm)
+        view_after_expire = self.get_request_param(c.view_after_expire_parm)
+        reveal_owner = self.get_request_param(c.reveal_owner_parm)
+        restrict_comms = self.get_request_param(c.restrict_comms_parm)
+        password = self.get_request_param(c.password_parm)
+
+        return create_conversation(user, name,
+                                   destroy_date, id_policy,
+                                   view_after_expire, reveal_owner,
+                                   restrict_comms, password)
 
     def put_hook(self, user, url_id):
         """Update conversation API"""
