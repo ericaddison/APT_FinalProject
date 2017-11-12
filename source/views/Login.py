f8b51f76fae76ad7c5f84eda389f887bdc821d04
Patrick Sigourney <psigourney@gmail.com>
2017-11-12 08:13:04 -0600

    Login seems to work

--- source/views/Login.py
+++ source/views/Login.py
@@ -4,9 +4,6 @@ from source.framework.BaseHandler import BaseHandler
 class LoginPage(BaseHandler):
     def post(self):
         template_values = {}
-        email = self.get_request_param('email')
-        fname = self.get_request_param('fname')
-        lname = self.get_request_param('lname')
         self.render_template('app/LandingPage.html', template_values)
 
 
