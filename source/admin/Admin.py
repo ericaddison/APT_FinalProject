bf559cc73c0bd150bd5dfab2037815aac2066b89
mspagon <mspagon@gmail.com>
2017-11-14 02:09:05 -0600

    Added AdminTemplate.html

--- source/admin/Admin.py
+++ source/admin/Admin.py
@@ -5,5 +5,5 @@ class AdminDashboard(BaseHandler):
     """Admin dashboard to allow admin and testing access"""
     def get(self):
 
-        template_values = {}
+        template_values = {'html_template': 'admin/AdminTemplate.html'}
         self.render_template('admin/AdminDashboard.html', template_values)
