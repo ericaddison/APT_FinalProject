be34319db20fbad1feb2543e8f216ccd97b1ce27
mspagon <mspagon@gmail.com>
2017-11-14 02:54:50 -0600

    Added more admin pages.

--- source/admin/Admin.py
+++ source/admin/Admin.py
@@ -1,9 +1,31 @@
 from source.framework.BaseHandler import BaseHandler
 
 
-class AdminDashboard(BaseHandler):
+class AdminLanding(BaseHandler):
     """Admin dashboard to allow admin and testing access"""
     def get(self):
 
         template_values = {'html_template': 'admin/AdminTemplate.html'}
-        self.render_template('admin/AdminDashboard.html', template_values)
+        self.render_template('admin/AdminLanding.html', template_values)
+
+
+class AdminConversations(BaseHandler):
+    """Admin dashboard to allow admin and testing access"""
+    def get(self):
+
+        template_values = {'html_template': 'admin/AdminTemplate.html'}
+        self.render_template('admin/AdminConversations.html', template_values)
+
+class AdminUsers(BaseHandler):
+    """Admin dashboard to allow admin and testing access"""
+    def get(self):
+
+        template_values = {'html_template': 'admin/AdminTemplate.html'}
+        self.render_template('admin/AdminUsers.html', template_values)
+
+class AdminMessages(BaseHandler):
+    """Admin dashboard to allow admin and testing access"""
+    def get(self):
+
+        template_values = {'html_template': 'admin/AdminTemplate.html'}
+        self.render_template('admin/AdminMessages.html', template_values)
