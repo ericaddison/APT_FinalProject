1efcecbf6b37e1489df67f8875c2f6245c1a73f1
ericaddison <ericaddison@gmail.com>
2017-11-17 01:31:59 -0600

    Added sendgrid sendmail function

---
+++ source/framework/SendGrid.py
@@ -0,0 +1,16 @@
+import sendgrid
+from source.config.authentication import SENDGRID_API_KEY, EMAIL_DOMAIN
+from sendgrid.helpers.mail import *
+import logging
+
+
+def send_email(to, sender, subject, text):
+    sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+    from_email = Email("{}@{}".format(sender, EMAIL_DOMAIN))
+    to_email = Email(to)
+    content = Content("text/plain", text)
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    logging.debug(response.status_code)
+    logging.debug(response.body)
+    logging.debug(response.headers)
